4cfba621ae2fdf0404b6b82755ca5e063c10d446
Sheroz Akram <sheroz.akram@outlook.com>
2024-02-01 13:09:54 +0500

    Add the word file

--- E-Learning.cpp
+++ E-Learning.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-class E-Learning{
-private:
-	Student * studentList;
-	Teacher * teacherList;
+class ItemCost {
 public:
-	E-Learning();
-	void enrollStudent(Student newStudent);
-	void addTeacher(Teacher newTeacher);
-	void assignTeacher(int studentID, int teacherID);
-	void login(string email, string password);
-};
-
-<<<<<<< HEAD
-void enrollStudent(Student newStudent){
-	studentList.add(newStudent);
+	int quantity;
+	double price;
+	void display(){
+		cout << ", Quantity:" << quantity << ", Price: " << price << endl;
+	}
+}
+class Product {
+public:
+	string name;
+	ItemCost itemTotalCost;
+	Product(string name, ItemCost itemCost): name(name), itemTotalCost(itemCost) {}
+	void displayProduct(){
+		cout << "Name: " << name;
+		itemTotalCost.display();
+	}
 }
 
-void addTeacher(Teacher newTeacher){
-	teacherList.add(newTeacher);
-=======
-void assignTeacher(int studentID, int teacherID){
-	Student student = studentList.findStudent(studentID);
-	student.assign(teacherID);
->>>>>>> feature/assignTeacher
+class InventoryStore{
+public:
+            vector<Product> products; 
+             void addProduct(Product newProduct){
+                      products.push_back(newProduct);
+             }
+}
+
+class DisplayInventory{
+public:
+            void display_inventory(vector<Product> * products){
+                     cout << “Inventory:\n”;
+                     for(auto &product : products){
+                               product.displayProduct();
+                     }
+            }
+
+}
+
+int main(){
+	Inventory inventory;
+	Product p1 = Product("item1, ItemCost(10, 5.99));
+	Product p2 = Product("item2, ItemCost(20, 10.99));
+	Product p3 = Product("item3, ItemCost(5, 15.99));
+	inventory.addProduct(p1);
+	inventory.addProduct(p2);
+	inventory.addProduct(p3);
+	
+	DisplayInventory displayInv();
+	displayInv.display_inventory(inventory.products);
 }
