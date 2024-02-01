630cc3407ba040168fd48ee2df5daa5a0db331fb
Sheroz Akram <sheroz.akram@outlook.com>
2024-02-01 12:00:43 +0500

    Implement the feature/addTeacher

--- E-Learning.cpp
+++ E-Learning.cpp
@@ -8,8 +8,10 @@ private:
 	Teacher * teacherList;
 public:
 	E-Learning();
-	public void enrollStudent(Student newStudent);
-	public void addTeacher(Teacher newTeacher);
-	public void assignTeacher(int studentID, int teacherID);
-	public void login(string email, string password);
+	void enrollStudent(Student newStudent);
+	void addTeacher(Teacher newTeacher){
+		teacherList.add(newTeacher);
+	}
+	void assignTeacher(int studentID, int teacherID);
+	void login(string email, string password);
 };
